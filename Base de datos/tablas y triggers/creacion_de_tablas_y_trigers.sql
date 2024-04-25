c53a544d1f89b5728d51b832ad6aeaf6b97f5dd2
Ander Arregui <ander.arregui@ikasle.egibide.org>
2024-04-25 14:06:50 +0200

    trigers y creates

--- Base de datos/tablas y triggers/creacion_de_tablas_y_trigers.sql
+++ Base de datos/tablas y triggers/creacion_de_tablas_y_trigers.sql
@@ -160,6 +160,20 @@ END;
 
 /*-------*/
 
+CREATE OR REPLACE TRIGGER bloqueo_competicion_cerrada
+BEFORE INSERT OR DELETE OR UPDATE ON EQUIPO
+FOR EACH ROW
+DECLARE
+    v_estado_competicion VARCHAR2(20);
+BEGIN
+    SELECT ESTADO INTO v_estado_competicion FROM COMPETICION WHERE ID_COMPETICION = :NEW.ID_COMPETICION;
+    
+    IF v_estado_competicion = 'cerrado' THEN
+        RAISE_APPLICATION_ERROR(-20001, 'No se pueden realizar operaciones en EQUIPO cuando la competición está cerrada.');
+    END IF;
+END;
+INSERT INTO JUGADOR (NOMBRE, APELLIDO1, APELLIDO2, SUELDO, NACIONALIDAD, FECHA_NACIMIENTO, NICKNAME, ROL, ID_EQUIPO) VALUES ('Jugador1', 'Apellido1', 'Apellido2', 1000, 'Nacionalidad1', TO_DATE('1990-11-01', 'YYYY-MM-DD'), 'Nick1', 'Rol1', 3);
+
 /*inserts*/
 
 
