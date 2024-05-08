82252acdeaae14638af6da1444e8e2afd31029e1
BryanAD <alzamoradiaz123@gmail.com>
2024-05-08 09:05:10 +0200

    A

--- Base de datos/Fase 2/triggers/modificacion_jugadores.sql
+++ Base de datos/Fase 2/triggers/modificacion_jugadores.sql
@@ -1,5 +1,5 @@
 /---CREACION DE TRIGGER QUE MIRA SI SE PUEDEN MoDIFICAR, A�ADIR O ELIMINAR LOS INTEGRANTES CUANDO LA COMPETICION SE HA INICIADO----/
-DROP TRIGGER TR_NO_INGR_JUGADORES;
+
 CREATE OR REPLACE TRIGGER TR_NO_INGR_JUGADORES
 BEFORE INSERT OR UPDATE OR DELETE ON JUGADOR
 FOR EACH ROW
@@ -8,11 +8,22 @@ DECLARE
   E_NO_INGR_JUG EXCEPTION;
 BEGIN
   SELECT c.estado INTO v_estado
+/*---CREACION DE TRIGGER QUE MIRA SI SE PUEDEN MoDIFICAR LOS JUGADORES CUANDO LA COMPETICION SE HA INICIADO----*/
+CREATE OR REPLACE TRIGGER lock_jugador_table
+BEFORE INSERT OR UPDATE or delete  ON jugador
+FOR EACH ROW
+DECLARE
+  v_estado VARCHAR2(20);
+BEGIN
+  SELECT c.estado
+  INTO v_estado
+
   FROM competicion c
   JOIN clasificacion cl ON c.id_competicion = cl.id_competicion
   WHERE cl.id_equipo = :NEW.id_equipo;
 
   IF v_estado = 'cerrado' THEN
+<<<<<<< HEAD
     RAISE_APPLICATION_ERROR(-20002, 'No se puede agregar o actualizar un jugador a un equipo que participa en una competici�n cerrada');
   END IF;
 
@@ -25,4 +36,22 @@ EXCEPTION
     RAISE_APPLICATION_ERROR(-20024, 'ERROR INESPERADO: ' || SQLCODE || ' - ' || SQLERRM);
 END TR_NO_INGR_JUGADORES;
 
+INSERT INTO JUGADOR (NOMBRE, APELLIDO1, APELLIDO2, SUELDO, NACIONALIDAD, FECHA_NACIMIENTO, NICKNAME, ROL, ID_EQUIPO)
+VALUES ('OL', 'Johnson', 'Brown', 2100, 'Inglaterra', TO_DATE('1991-12-03', 'YYYY-MM-DD'), 'OL', 'Defensa', 5);
 
+INSERT INTO JUGADOR (NOMBRE, APELLIDO1, APELLIDO2, SUELDO, NACIONALIDAD, FECHA_NACIMIENTO, NICKNAME, ROL, ID_EQUIPO)
+VALUES ('Harper', 'Martinez', 'Rodriguez', 1800, 'Francia', TO_DATE('1992-08-20', 'YYYY-MM-DD'), 'Harper', 'Portero', 5);
+
+DELETE FROM JUGADOR
+WHERE ID_INTEGRANTE = 22;
+
+UPDATE JUGADOR
+SET NOMBRE = 'PABLITO',
+    APELLIDO1 = 'PEREZ'
+WHERE ID_INTEGRANTE = 22;
+=======
+    RAISE_APPLICATION_ERROR(-20002, 'No se puede agregar o actualizar un jugador a un equipo que participa en una competición cerrada');
+  END IF;
+END lock_jugador_table;
+
+>>>>>>> a503a61b74788a5d1bc1f0aebb88c6ec40054d7e
