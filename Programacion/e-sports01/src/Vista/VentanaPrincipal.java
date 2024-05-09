05320f52e1748413fa3afab7555b93c8b2d5f43d
Ander Arregui <ander.arregui@ikasle.egibide.org>
2024-05-09 10:27:28 +0200

    prinicipo de proyecto java

---
+++ Programacion/e-sports01/src/Vista/VentanaPrincipal.java
@@ -0,0 +1,17 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JPanel panel1;
+    private JButton CLASIFICACIONButton;
+    private JButton ULTIMAJORNADAButton;
+    private JButton EQUIPOSButton;
+    private JButton TODASJORNADASButton;
+    private JButton GESTIONDEUSUARIOSButton;
+    private JButton COMPETICIONESButton;
+    private JButton CERRARSESIONButton;
+    private JButton SALIRButton;
+
+
+}
