3ece9b006af2cf2dc380b953abd3da001e21c269
Oier <oier.albeniz@ikasle.egibide.org>
2024-05-14 10:43:24 +0200

    Ventana Jugador

---
+++ Programacion/e-sports01/src/Vista/VentanaJugador.java
@@ -0,0 +1,47 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VentanaJugador extends JDialog {
+    private JPanel contentPane;
+    private JPanel panelConsulta;
+    private JTextField tfNombreEquipoConsulta;
+    private JTextArea taDatosConsulta;
+    private JButton bModificar;
+    private JButton bCancelarModificacion;
+    private JPanel panelAlta;
+    private JTextField tfApellido1;
+    private JButton bCancelarAlta;
+    private JButton bInscribirJugador;
+    private JRadioButton rbAlta;
+    private JRadioButton rbModificacion;
+    private JRadioButton rbConsulta;
+    private JRadioButton rbBaja;
+    private JPanel panelBaja;
+    private JTextField tfNombreJugadorBaja;
+    private JTextArea taDatosJugador;
+    private JButton bCancelarBaja;
+    private JButton bBorrarJugador;
+    private JTextField tfApellido2;
+    private JTextField tfNacionalidad;
+    private JTextField tfFechaNacimiento;
+    private JTextField tfSueldo;
+    private JTextField tfNombreJugadorAlta;
+    private JTextField tfNickname;
+    private JTextField tfRol;
+    private JComboBox cbEquipos;
+    private JTextField tfNombreJugadorModificacion;
+    private JTextField tfApellido1Modificacion;
+    private JTextField tfApellido2Modificacion;
+    private JTextField tfSueldoModificacion;
+    private JTextField tfNacionalidadModificacion;
+    private JTextField tfFechaNacimientoModificacion;
+    private JTextField tfNicknameModificacion;
+    private JTextField tfRolModificacion;
+    private JComboBox cb;
+    private JButton bSalirConsulta;
+
+    public VentanaJugador() {
+        
+    }
+}
