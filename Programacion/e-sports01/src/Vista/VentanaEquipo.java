1813724c59e13aa1dc6ae1d17531ca8812750250
Ander Arregui <ander.arregui@ikasle.egibide.org>
2024-05-14 09:17:53 +0200

    creacion de vista equipo

--- Programacion/e-sports01/src/Vista/VentanaEquipo.java
+++ Programacion/e-sports01/src/Vista/VentanaEquipo.java
@@ -1,4 +1,17 @@
 package Vista;
 
+import javax.swing.*;
+
 public class VentanaEquipo {
+    private JRadioButton altaRadioButton;
+    private JRadioButton modificacionRadioButton;
+    private JRadioButton bajaRadioButton;
+    private JRadioButton consultaRadioButton;
+    private JPanel panelAlta;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JButton cancelarButton;
+    private JButton inscribirButton;
+    private JComboBox comboBox2;
 }
